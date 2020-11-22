1361400449fff62fea7b784f5bd35904a3e80f2b
Sample <royanjos279@gmail.com>
2020-11-21 20:49:07 -0800

    Just Now

---
+++ src/org/test/A.java
@@ -0,0 +1,22 @@
+package org.test;
+
+public class A {
+	public static void main(String[] args) {
+		String a = "Welcome to java class";
+		
+		String substring = a.substring(0,7);
+		
+		
+	
+		
+		
+		
+		
+	}
+	
+
+			
+	}
+
+
+
